5acff8e787fd9d77acd94f7d95aed3ee77b68488
YujiaY <jack.yujia.yuan@gmail.com>
2017-04-06 10:50:38 +0800

    Initially finished with tut

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'auth/dashboard'
+
+  get 'auth/landing'
+
+  root to: 'auth#dashboard'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
